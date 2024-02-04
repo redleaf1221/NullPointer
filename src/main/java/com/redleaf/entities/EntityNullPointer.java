af766f1bc85efe6ff2fd71ad873daf7d701b9156
qwqpwpqvq <qwqpwpqvq@163.com>
2024-02-04 12:58:40 +0800

    NullPointer实体修改

--- src/main/java/com/redleaf/entities/EntityNullPointer.java
+++ src/main/java/com/redleaf/entities/EntityNullPointer.java
@@ -1,12 +1,36 @@
 package com.redleaf.entities;
 
-
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Arm;
 import net.minecraft.world.World;
 
-public class EntityNullPointer extends PathAwareEntity {
-    public EntityNullPointer(EntityType<? extends PathAwareEntity> entityType, World world) {
+public class EntityNullPointer extends LivingEntity {
+    public EntityNullPointer(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
     }
+
+    @Override
+    public Iterable<ItemStack> getArmorItems() {
+        return null;
+    }
+
+    @Override
+    public ItemStack getEquippedStack(EquipmentSlot slot) {
+        return null;
+    }
+
+    @Override
+    public void equipStack(EquipmentSlot slot, ItemStack stack) {
+
+    }
+
+    @Override
+    public Arm getMainArm() {
+        return null;
+    }
+
+
 }
--- src/main/java/com/redleaf/entities/EntityRegistry.java
+++ src/main/java/com/redleaf/entities/EntityRegistry.java
@@ -1,6 +1,6 @@
 package com.redleaf.entities;
 
-import com.redleaf.NullPointer;
+
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityDimensions;
@@ -11,12 +11,11 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class EntityRegistry {
-    public static final EntityType<EntityNullPointer> ENullPointer = Registry.register(
+    public static final EntityType<EntityNullPointer> NULL_POINTER_ENTITY = Registry.register(
             Registries.ENTITY_TYPE,
-            new Identifier(NullPointer.MODID, "null_pointer"),
-            FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, EntityNullPointer::new).dimensions(EntityDimensions.fixed(0.75f, 0.75f)).build());
-
-    public static void register() {
-        FabricDefaultAttributeRegistry.register(ENullPointer, EntityNullPointer.createMobAttributes());
+            new Identifier("nullpointer", "null_pointer"),
+            FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, EntityNullPointer::new).dimensions(EntityDimensions.fixed(0.25f, 0.25f)).build());
+    public static void register(){
+        FabricDefaultAttributeRegistry.register(NULL_POINTER_ENTITY, EntityNullPointer.createLivingAttributes());
     }
 }
