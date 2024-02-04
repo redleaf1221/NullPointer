f52dcd6a52365d63b79eeb1a9602053955be7935
qwqpwpqvq <qwqpwpqvq@163.com>
2024-02-04 14:57:46 +0800

    增加pointer的功能

--- src/main/java/com/redleaf/items/ItemPointer.java
+++ src/main/java/com/redleaf/items/ItemPointer.java
@@ -1,10 +1,33 @@
 package com.redleaf.items;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
-
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 public class ItemPointer extends Item {
 
     public ItemPointer(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context){
+        PlayerEntity player = context.getPlayer();
+        World world = context.getWorld();
+        BlockPos pos = context.getBlockPos();
+        if (!world.isClient){
+            BlockState state = world.getBlockState(pos);
+            BlockEntity entity = world.getBlockEntity(pos);
+
+
+            player.sendMessage(Text.literal(state.toString()));
+            player.sendMessage(Text.literal(entity.toString()));
+        }
+        return ActionResult.success(world.isClient());
     }
 }
