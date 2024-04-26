668c84aee90c18ad437216f093173a5e511c91e7
qwqpwpqvq <qwqpwpqvq@163.com>
2024-04-26 20:55:25 +0800

    修改pointer

--- src/main/java/com/redleaf/items/ItemPointer.java
+++ src/main/java/com/redleaf/items/ItemPointer.java
@@ -4,6 +4,9 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.nbt.NbtString;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
@@ -22,16 +25,38 @@ public class ItemPointer extends Item {
         PlayerEntity player = context.getPlayer();
         World world = context.getWorld();
         BlockPos pos = context.getBlockPos();
-        if (player == null) return ActionResult.PASS;
-        if (world.isClient()) return ActionResult.success(true);
-        if (!this.haveData) {
-            player.sendMessage(Text.translatable("item.nullpointer.pointer.tip1", pos.getX(), pos.getY(), pos.getZ()));
-            this.blockPos = pos;
-            this.haveData = true;
-        } else {
-            player.sendMessage(Text.translatable("item.nullpointer.pointer.tip2"));
-            this.blockPos = null;
-            this.haveData = false;
+        ItemStack stack = context.getStack();
+        // 如果玩家在 潜行 时 use 方块
+        if (player != null && player.isSneaking()) {
+            // 保存方块位置
+            String worldKey = world.getRegistryKey().getValue().toString();
+            NbtCompound nbt = stack.getOrCreateNbt();
+            nbt.putString("world", worldKey);
+            nbt.putInt("x", pos.getX());
+            nbt.putInt("y", pos.getY());
+            nbt.putInt("z", pos.getZ());
+
+            // 添加 Lore
+            NbtList loreList = new NbtList();
+            loreList.add(NbtString.of("{\"text\":\"world: " + worldKey + "\"}"));
+            loreList.add(NbtString.of("{\"text\":\"X: " + pos.getX() + "\"}"));
+            loreList.add(NbtString.of("{\"text\":\"Y: " + pos.getY() + "\"}"));
+            loreList.add(NbtString.of("{\"text\":\"Z: " + pos.getZ() + "\"}"));
+
+            NbtCompound display = new NbtCompound();
+            display.put("Lore", loreList);
+            nbt.put("display", display);
+
+            nbt.putBoolean("PointTo", true);
+            stack.setNbt(nbt);
+
+        } else if (player != null) {
+            // 检测是否有方块位置
+            if (stack.getNbt() != null && stack.getNbt().getBoolean("PointTo")) {
+                // 获取方块位置
+                NbtCompound nbt = stack.getNbt();
+                player.sendMessage(Text.of(nbt.toString()));
+            }
         }
         return ActionResult.success(false);
     }
